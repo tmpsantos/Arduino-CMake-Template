8dfd72141fc2d0829cd1ff934ff1d9a52df19a1b
Markus Bader <markus.bader@tuwien.ac.at>
2015-08-20 09:41:08 +0200

    Working with Arduino IDE 1.6.5 and Arduino UNO

--- scripts/arduino.cmake
+++ scripts/arduino.cmake
@@ -4,34 +4,52 @@
 # http://johanneshoff.com/arduino-command-line.html
 # http://www.arduino.cc/playground/Code/CmakeBuild
 # http://www.tmpsantos.com.br/en/2010/12/arduino-uno-ubuntu-cmake/
+# http://forum.arduino.cc/index.php?topic=244741.0
+
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_C_COMPILER avr-gcc)
 set(CMAKE_CXX_COMPILER avr-g++)
 set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
+enable_language(ASM)
 
 # C only fine tunning
 set(TUNNING_FLAGS "-funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums") 
 
-set(CMAKE_CXX_FLAGS "-mmcu=${ARDUINO_BOARD} -DF_CPU=${ARDUINO_FCPU} -Os")
+set(CMAKE_CXX_FLAGS "-mmcu=${ARDUINO_MCU} -DF_CPU=${ARDUINO_FCPU} -Os")
 set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} ${TUNNING_FLAGS} -Wall -Wstrict-prototypes -std=gnu99")
 
-set(ARDUINO_CORE_DIR "/usr/share/arduino/hardware/arduino/cores/arduino/")
+set(ARDUINO_CORE_DIR "${ARDUINO_ROOT}/hardware/arduino/avr/cores/arduino/")
+set(ARDUINO_PINS_DIR "${ARDUINO_ROOT}/hardware/arduino/avr/variants/${ARDUINO_BOARD}")
+set(ARDUINO_PINS_DIR "${ARDUINO_ROOT}/hardware/arduino/avr/variants/${ARDUINO_BOARD}")
+set(AVRDUDE_CONFIG "${ARDUINO_ROOT}/hardware/tools/avr/etc/avrdude.conf")
+
+include_directories(${ARDUINO_PINS_DIR})
 include_directories(${ARDUINO_CORE_DIR})
 
 set(ARDUINO_SOURCE_FILES
-    ${CMAKE_SOURCE_DIR}/scripts/ldd_hack.cpp
-	${ARDUINO_CORE_DIR}/HardwareSerial.cpp
-	${ARDUINO_CORE_DIR}/pins_arduino.c
-	${ARDUINO_CORE_DIR}/Print.cpp
-	${ARDUINO_CORE_DIR}/WInterrupts.c
-	${ARDUINO_CORE_DIR}/wiring_analog.c
-	${ARDUINO_CORE_DIR}/wiring.c
+	${ARDUINO_CORE_DIR}/wiring_pulse.S
 	${ARDUINO_CORE_DIR}/wiring_digital.c
+	${ARDUINO_CORE_DIR}/wiring.c
+	${ARDUINO_CORE_DIR}/WInterrupts.c
 	${ARDUINO_CORE_DIR}/wiring_pulse.c
 	${ARDUINO_CORE_DIR}/wiring_shift.c
+	${ARDUINO_CORE_DIR}/hooks.c 
+	${ARDUINO_CORE_DIR}/wiring_analog.c
 	${ARDUINO_CORE_DIR}/WMath.cpp
+	${ARDUINO_CORE_DIR}/IPAddress.cpp
+	${ARDUINO_CORE_DIR}/Tone.cpp
+	${ARDUINO_CORE_DIR}/HardwareSerial2.cpp
+	${ARDUINO_CORE_DIR}/HID.cpp
+	${ARDUINO_CORE_DIR}/Print.cpp
+	${ARDUINO_CORE_DIR}/new.cpp
+	${ARDUINO_CORE_DIR}/HardwareSerial0.cpp
+	${ARDUINO_CORE_DIR}/HardwareSerial.cpp
 	${ARDUINO_CORE_DIR}/WString.cpp
+	${ARDUINO_CORE_DIR}/abi.cpp
+	${ARDUINO_CORE_DIR}/USBCore.cpp
+	${ARDUINO_CORE_DIR}/Stream.cpp
+	${ARDUINO_CORE_DIR}/CDC.cpp
 )
 
 set(PORT $ENV{ARDUINO_PORT})
@@ -55,7 +73,7 @@ if(AVROBJCOPY AND AVRDUDE)
     add_dependencies(flash hex)
 
     add_custom_command(TARGET flash POST_BUILD
-        COMMAND ${AVRDUDE} -P ${PORT} -b ${ARDUINO_UPLOAD_SPEED} -c ${ARDUINO_PROTOCOL} -p ${ARDUINO_MCU} -V -F -U flash:w:firmware.hex:i
+        COMMAND ${AVRDUDE} -C${AVRDUDE_CONFIG} -v -p${ARDUINO_MCU} -c${ARDUINO_PROTOCOL}  -P${PORT} -b${ARDUINO_UPLOAD_SPEED} -D -Uflash:w:firmware.hex:i
     )
 endif()
 
--- scripts/ldd_hack.cpp
+++
@@ -1,5 +0,0 @@
-// XXX: See Robzed's tip about this issue
-// http://zedcode.blogspot.com/2007/02/gcc-c-link-problems-on-small-embedded.html
-extern "C" void __cxa_pure_virtual(void)
-{
-}
