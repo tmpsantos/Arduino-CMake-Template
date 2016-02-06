96045529b9d612d58f94cf099629cf7e966d35bd
Matthijs Oosterhoff <matthijs.oosterhoff@gmail.com>
2016-02-06 17:07:26 +0100

    Simplified USBasp impl.

--- cmake/arduino-1.6.cmake
+++ cmake/arduino-1.6.cmake
@@ -15,7 +15,7 @@ set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
 enable_language(ASM)
 
 # C only fine tunning
-set(TUNNING_FLAGS "-funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums") 
+set(TUNNING_FLAGS "-funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums")
 
 set(CMAKE_CXX_FLAGS "-mmcu=${ARDUINO_MCU} -DF_CPU=${ARDUINO_FCPU} -Os")
 set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} ${TUNNING_FLAGS} -Wall -Wstrict-prototypes -std=gnu99")
@@ -30,67 +30,67 @@ include_directories(${ARDUINO_PINS_DIR})
 include_directories(${ARDUINO_CORE_DIR})
 
 set(ARDUINO_SOURCE_FILES
-		${ARDUINO_CORE_DIR}/wiring_pulse.S
+        ${ARDUINO_CORE_DIR}/wiring_pulse.S
 
-		${ARDUINO_CORE_DIR}/WInterrupts.c
-		${ARDUINO_CORE_DIR}/hooks.c
-		${ARDUINO_CORE_DIR}/wiring.c
-		${ARDUINO_CORE_DIR}/wiring_analog.c
-		${ARDUINO_CORE_DIR}/wiring_digital.c
-		${ARDUINO_CORE_DIR}/wiring_pulse.c
-		${ARDUINO_CORE_DIR}/wiring_shift.c
+        ${ARDUINO_CORE_DIR}/WInterrupts.c
+        ${ARDUINO_CORE_DIR}/hooks.c
+        ${ARDUINO_CORE_DIR}/wiring.c
+        ${ARDUINO_CORE_DIR}/wiring_analog.c
+        ${ARDUINO_CORE_DIR}/wiring_digital.c
+        ${ARDUINO_CORE_DIR}/wiring_pulse.c
+        ${ARDUINO_CORE_DIR}/wiring_shift.c
 
-		${ARDUINO_CORE_DIR}/CDC.cpp
-		${ARDUINO_CORE_DIR}/HardwareSerial.cpp
-		${ARDUINO_CORE_DIR}/HardwareSerial0.cpp
-		${ARDUINO_CORE_DIR}/HardwareSerial1.cpp
-		${ARDUINO_CORE_DIR}/HardwareSerial2.cpp
-		${ARDUINO_CORE_DIR}/HardwareSerial3.cpp
-		${ARDUINO_CORE_DIR}/IPAddress.cpp
-		${ARDUINO_CORE_DIR}/PluggableUSB.cpp
-		${ARDUINO_CORE_DIR}/Print.cpp
-		${ARDUINO_CORE_DIR}/Stream.cpp
-		${ARDUINO_CORE_DIR}/Tone.cpp
-		${ARDUINO_CORE_DIR}/USBCore.cpp
-		${ARDUINO_CORE_DIR}/WMath.cpp
-		${ARDUINO_CORE_DIR}/WString.cpp
-		${ARDUINO_CORE_DIR}/abi.cpp
-		${ARDUINO_CORE_DIR}/new.cpp
-)
+        ${ARDUINO_CORE_DIR}/CDC.cpp
+        ${ARDUINO_CORE_DIR}/HardwareSerial.cpp
+        ${ARDUINO_CORE_DIR}/HardwareSerial0.cpp
+        ${ARDUINO_CORE_DIR}/HardwareSerial1.cpp
+        ${ARDUINO_CORE_DIR}/HardwareSerial2.cpp
+        ${ARDUINO_CORE_DIR}/HardwareSerial3.cpp
+        ${ARDUINO_CORE_DIR}/IPAddress.cpp
+        ${ARDUINO_CORE_DIR}/PluggableUSB.cpp
+        ${ARDUINO_CORE_DIR}/Print.cpp
+        ${ARDUINO_CORE_DIR}/Stream.cpp
+        ${ARDUINO_CORE_DIR}/Tone.cpp
+        ${ARDUINO_CORE_DIR}/USBCore.cpp
+        ${ARDUINO_CORE_DIR}/WMath.cpp
+        ${ARDUINO_CORE_DIR}/WString.cpp
+        ${ARDUINO_CORE_DIR}/abi.cpp
+        ${ARDUINO_CORE_DIR}/new.cpp
+        )
 
 set(PORT $ENV{ARDUINO_PORT})
 if (NOT PORT)
     set(PORT ${ARDUINO_PORT})
-endif()
+endif ()
 
 find_program(AVROBJCOPY ${ARDUINO_ROOT}/hardware/tools/avr/bin/avr-objcopy)
 find_program(AVRDUDE ${ARDUINO_ROOT}/hardware/tools/avr/bin/avrdude)
 
 # FIXME: Forcing target name to be "firmware"
-if(AVROBJCOPY AND AVRDUDE)
+if (AVROBJCOPY AND AVRDUDE)
     add_custom_target(hex)
     add_dependencies(hex firmware)
 
     add_custom_command(TARGET hex POST_BUILD
-        COMMAND ${AVROBJCOPY} -O ihex -R .eeprom ${CMAKE_CURRENT_BINARY_DIR}/firmware firmware.hex
-    )
+            COMMAND ${AVROBJCOPY} -O ihex -R .eeprom ${CMAKE_CURRENT_BINARY_DIR}/firmware firmware.hex
+            )
 
     add_custom_target(flash)
     add_dependencies(flash hex)
 
-    add_custom_command(TARGET flash POST_BUILD
-        COMMAND ${AVRDUDE} -C${AVRDUDE_CONFIG} -v -p${ARDUINO_MCU} -c${ARDUINO_PROTOCOL}  -P${PORT} -b${ARDUINO_UPLOAD_SPEED} -D -Uflash:w:firmware.hex:i
-    )
+    if (ARDUINO_PROTOCOL STREQUAL "usbasp")
+        add_custom_command(TARGET flash POST_BUILD
+                COMMAND ${AVRDUDE} -C${AVRDUDE_CONFIG} -v -p${ARDUINO_MCU} -c usbasp -D -Uflash:w:firmware.hex:i
+                )
+    else ()
+        add_custom_command(TARGET flash POST_BUILD
+                COMMAND ${AVRDUDE} -C${AVRDUDE_CONFIG} -v -p${ARDUINO_MCU} -c${ARDUINO_PROTOCOL} -P${PORT} -b${ARDUINO_UPLOAD_SPEED} -D -Uflash:w:firmware.hex:i
+                )
+    endif ()
 
-	add_custom_target(flash-usbasp)
-	add_dependencies(flash-usbasp hex)
-
-	add_custom_command(TARGET flash-usbasp POST_BUILD
-		COMMAND ${AVRDUDE} -C${AVRDUDE_CONFIG} -v -p${ARDUINO_MCU} -c usbasp -D -Uflash:w:firmware.hex:i
-	)
-endif()
+endif ()
 
 add_custom_target(reset)
 add_custom_command(TARGET reset POST_BUILD
-    COMMAND echo 0 > ${PORT}
-)
+        COMMAND echo 0 > ${PORT}
+        )
